f1550eb475f41abca63f6f17afd043ad7491fe9f
Youssef <ybkstudies@gmail.com>
2021-04-07 19:54:13 +0200

    empiric sorts

--- S4/I41-Algo/TP7/sujet1.c
+++ S4/I41-Algo/TP7/sujet1.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define True 1
+#define False 0
+
 typedef unsigned int uint;
+typedef unsigned long long ullong;
 
 void swap(uint *arr, uint i, uint j) {
     uint tmp;
@@ -11,10 +15,137 @@ void swap(uint *arr, uint i, uint j) {
     arr[j] = tmp;
 }
 
-uint* gen_perm(uint *arr, uint n) {
-    srand(time(NULL));
-    for (uint i = 0; i < n; i++) {
-        uint tmp = i + rand() % n;
-        swap(arr, tmp, n-1);    
+void print_array(uint *arr, uint n) {
+    printf("[");
+    for (uint i = 0; i < n-1; i++) {
+        printf("%d,", arr[i]);
+    }
+    printf("%d]\n", arr[n-1]);
+}
 
+void create_arr(uint *arr, uint n) {
+    for (uint i = 0; i < n; i++) {
+        arr[i] = i;
+    }
+}
+
+void copy_arr(uint *arr, uint *new_arr, uint n) {
+    for (uint i = 0; i < n; i++) {
+        new_arr[i] = arr[i];
+    }
+}
+
+uint* gen_perm(uint n) {
+    uint *new_arr = (uint *) malloc(sizeof(uint)*n);
+    create_arr(new_arr, n);
+    uint tmp;
+
+    for (uint i = 0; i < n; i++) {
+        do {
+            tmp = rand() % n;
+        } while (tmp < i);
+        swap(new_arr, i, tmp);    
+    }
+    return new_arr;
+}
+
+uint idx_min(int *arr, uint a, uint b) {
+    uint min = arr[a], min_idx = a;
+    for (uint i = a+1; i < b+1; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+            min_idx = i;
+        }
+    }
+    return min_idx;
+}
+        
+ullong selection_sort(int *arr, uint n) {
+    int *new_arr = (int *) malloc(sizeof(int)*n);
+    copy_arr(arr, new_arr, n);
+    int tmp;
+    ullong comp = 0;
+    for (uint i = 0; i < n; i++) {
+        tmp = idx_min(new_arr, i, n-1);
+        swap(new_arr, i, tmp);
+        comp += (n-i);
+    }
+    print_array(new_arr, n);
+    printf("tri selection fait ^^\n");
+    free(new_arr);
+    return comp;
+}
+
+ullong buble_sort(int *arr, uint n) {
+    int *new_arr = (int *) malloc(sizeof(int)*n);
+    copy_arr(arr, new_arr, n);
+    uint i = 0;
+    ullong comp = 0;
+    int flag = True;
+    while (i < n && flag) {
+        flag = False;
+        for (uint j = 1; j < (n-i); j++) {
+            if (new_arr[j-1] > new_arr[j]) {
+                swap(new_arr, j-1, j);
+                flag = True;
+            }
+            comp += 1;
+        }
+        i++;
+    }
+    print_array(new_arr, n);
+    printf("tri a bulles fait ^^\n");
+    free(new_arr);
+    return comp;
+}
+
+ullong insertion_sort(int *arr, uint n) {
+    int *new_arr = (int *) malloc(sizeof(int)*n);
+    copy_arr(arr, new_arr, n);
+    ullong comp = 0;
+    for (uint i = 1; i < n; i++) {
+        for (uint j = i; j > 0 && new_arr[j-1] > new_arr[j]; j--) {
+            swap(new_arr, j-1, j);
+            comp += 1;
+        }
+    }
+    print_array(new_arr, n);
+    printf("tri insertion fait ^^\n");
+    free(new_arr);
+    return comp;
+}
+            
+ullong fact(uint n) {
+    ullong res = 1;
+    for (int i = 2; i < n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+
+void main(int argc, char **argv) {
+    uint n = atoi(argv[1]);
+    ullong fact_n = fact(n), comp_sel = 0, comp_bub = 0, comp_ins = 0;
+    uint *arr;
+    srand(time(NULL));
+
+    FILE *file = fopen("tris_comp", "w");
+
+    for (uint i = 0; i < fact_n; i++) {
+        arr = gen_perm(n);
+
+        comp_sel = selection_sort(arr, n);
+
+        comp_bub = buble_sort(arr, n);
+
+        comp_ins = insertion_sort(arr, n);
+
+        free(arr);
+
+        fprintf(file, "%d %llu %llu %llu\n", i+1, comp_sel, comp_bub, comp_ins); 
+
+    }
+    fclose(file);
+}
     
