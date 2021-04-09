e7c67bd8c67e6540dee000e112d32d2e53f55cef
Youssef <ybkstudies@gmail.com>
2021-04-09 16:12:19 +0200

    crypto

--- S4/I43-Cybersecurite/cesar.py
+++ S4/I43-Cybersecurite/cesar.py
@@ -2,16 +2,57 @@ alphabet = []
 for _ in range(26):
     alphabet.append(chr(97+_));
 
+def cypher_cesar(m, k):
+    k = alphabet.index(k)
+    msg = ""
+    for letter in m:
+        num_letter = alphabet.index(letter);
+        msg += alphabet[(num_letter + k) % 26]
+    return msg
 
-def decypher(m, k):
+
+def decypher_cesar(m, k):
+    k = alphabet.index(k)
     msg_decyphered = ""
-    for i in range(len(m)):
-        num_lettre = alphabet.index(m[i]);
-        msg_decyphered += alphabet[(num_lettre - k) % 26]
+    for letter in m:
+        num_letter = alphabet.index(letter);
+        msg_decyphered += alphabet[(num_letter - k) % 26]
     return msg_decyphered
 
-print(decypher("mrmydmk", alphabet.index('m') - alphabet.index('e')))
-print(decypher("bgbnsb",alphabet.index('b') - alphabet.index('e')))
-print(decypher("tytfkt", alphabet.index('t') - alphabet.index('e')))
-print(decypher("glnsxz", 5))
-print(decypher("mrtydf", 5))
+def frequent_letter(m):
+    dico = {}
+    for letter in m:
+        if dico.get(letter):
+            dico[letter] += 1
+        else:
+            dico[letter] = 1
+
+    max_l = m[0]
+    max_f = dico[m[0]]
+    list_letters = []
+
+    for letter, frequency in dico.items():
+        if frequency > max_f:
+            max_f = frequency
+            max_l = letter
+    list_letters.append(max_l)
+
+    for letter, frequency in dico.items():
+        if frequency == max_f and letter not in list_letters:
+            list_letters.append(letter)
+
+    return list_letters[0]
+
+
+
+"""
+m = "olgtoylaldqlterclyonszdptwlufdepncppfypxmcjzyopaczrclxxpxltdnpaczrclxxpdpdeopgpwzaapwftxpxpnzxxpwzcotylepfcopolgtoypeltealddfqqtdlyetwlfetwtdpwpcpdplfazfcdtydelwwpcdfcwpdlfecpdzcotylepfcdtwlrclyotlwzcdopxlytpcppiazypyetpwwppewpgztwlacpwfopnzyypneplezfdwpdzcotylepfcdpenlalmwpopwpfcozyypcwpdzcocpdbftwgpfe"
+print(m)
+list_letter = frequent_letter(m)
+print(list_letter)
+for letter in list_letter:
+    possible_key = alphabet[(alphabet.index(letter) - alphabet.index('e')) % 26]
+    print(decypher(m,possible_key))
+
+print(alphabet, '\n', m,'\n',cypher(m,'h'), '\n', decypher(cypher(m, 'h'), 'h'), '\n')
+"""
--- S4/I43-Cybersecurite/vigenere.py
+++ S4/I43-Cybersecurite/vigenere.py
@@ -1,15 +1,87 @@
+from math import gcd
+from cesar import cypher_cesar, decypher_cesar, frequent_letter
+
 alphabet = []
 for i in range(26):
     alphabet.append(chr(97+i));
 
-
-def decypher(msg, key):
-    msg_decyphered = ""
+def cypher_vigenere(msg, key):
+    cyphered_msg = ""
     for i in range(len(msg)):
         key_int = alphabet.index(key[i % len(key)])
         char_int = alphabet.index(msg[i])
-        msg_decyphered += alphabet[(char_int - key_int) % 26]
-    return msg_decyphered
-print(decypher("ykscetktgipiwvkopriosxurmcjezwqe", "exofacile"))
+        cyphered_msg += alphabet[(char_int + key_int) % 26]
+    return cyphered_msg
 
 
+
+def decypher_vigenere(msg, key):
+    decyphered_msg = ""
+    for i in range(len(msg)):
+        key_int = alphabet.index(key[i % len(key)])
+        char_int = alphabet.index(msg[i])
+        decyphered_msg += alphabet[(char_int - key_int) % 26]
+    return decyphered_msg
+
+"""
+m = "uyhxcwtsohwzsfayiezvlduqejgomxtvekpjjgstgdkyjvcgdhbtvfwyhldgvudwogepcylocirtnzciitvfjytrwqceysijrswltikelhuqpvfpllpmkgcdyhmehzjgpxzqykujrvxtlyhwvlleuxwrvewccxvvzmmaijqcvccekgfjmtrvvlaycxvsfajtwcgnjypxvwcgzumtkpdfthiuewqpvkflnchrvvlanqmvpdmleejcfuijvrpevyaegtpkycgvfphltplfpvucwjqyhldnvvajyayuglnuxxjkxhftqvpekovkvtpdyviigxwhielfcwhbsukqaucxcgrwltqvpekyhhfefeycxj"
+k = "supercl"
+"""
+#print(frequent_letter(m))
+#print(decypher(m, k))
+#print(cypher(decypher(m, k), k) == m)
+
+
+def len_key(l, msg):
+    dico = {}
+    max_nplet = 0
+    for i in range(len(msg)-l+1):
+        if dico.get(msg[i:i+l]):
+            dico[msg[i:i+l]] += 1
+            if dico[msg[i:i+l]] > max_nplet:
+                max_nuplet = dico[msg[i:i+l]]
+                max_pattern = msg[i:i+l]
+        else:
+            dico[msg[i:i+l]] = 1
+
+    l_index = []
+    i = 0
+    while i < len(msg)-l+1:
+        if max_pattern == msg[i:i+l]:
+            l_index.append(i)
+            i = i+l
+        else:
+            i += 1
+
+    gcd1 = l_index[0]
+    for i in range(1,len(l_index)):
+        gcd1 = gcd(gcd1, l_index[i])
+    return gcd1
+
+
+
+
+    #print(dico, max_pattern, l_index)
+
+def kasiski(l,m):
+    key_l = len_key(l,m)
+    print(key_l)
+    sub_str = [""]*(len(m)//key_l-1)
+    for i in range(len(m)):
+        sub_str[i%key_l] += m[i]
+    sub_str_dec = []
+    for i in range(len(sub_str)):
+        print('----',sub_str)
+        l_freq = frequent_letter(sub_str[i])
+        key = alphabet.index(l_freq)
+        e = alphabet.index('e')
+        possible_key = alphabet[(key - e) % 26]
+        sub_str_dec.append(decypher_cesar(sub_str[i], possible_key))
+    print(sub_str_dec)
+
+m = "pbcjbjcrgwisiixrbrgxwquplcptgfiaeeamcftxqbnvisiyceteadxxfqwxgbsvnrzhkyjoeoxrqpckwrbummquvbskmcbwrngfijenxxqrgwyjeuledeeeefrrxyqswbzxnvlemoubxfopweksexpriebiqrgkijpntgbertvaeueiqttxwaihyioepmipcqgxoakkijepmernurwqeoxhbcjbjcrgfiktohrlanilxbgmmnugvsjmgeizhkyjoefxgbsckurinnxflklizerxraapmglmoxglmrhwxnvnraeetpxgg"
+m1 = "mbtgmrgylrmbtntynfsydskxdmbtzfk"
+print(len_key(2,m))
+
+print(len_key(2,m1))
+#kasiski(2,m)
