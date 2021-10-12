40292013cbca138e78228bfad096ab9f951decd1
dxkkxn <ybkstudies@gmail.com>
2021-10-12 19:35:47 +0200

    fixed grep

--- L3/S5/I53-Theorie-des-langages/TP3/mygrep.c
+++ L3/S5/I53-Theorie-des-langages/TP3/mygrep.c
@@ -1,6 +1,8 @@
 # include <regex.h>
 # include <stdio.h>
 # include <stdlib.h>
+# define true 1
+# define false 0
 int main ( int argc , char * argv[])
 {
     if (argc < 3) {
@@ -16,20 +18,21 @@ int main ( int argc , char * argv[])
     char to_p = ' ';
     int i;
     int regex_success, print_n;
+    int slice_start;
 
     if (regcomp(&regex, argv[1], REG_EXTENDED )!=0)
         return 1;
 
     while (getline(&line, &len,file) != -1) {
-        //printf("line read\n");
-        //fflush(stdout);
         regex_success = regexec(&regex, line, 1, &pmatch, 0); 
         if(regex_success == 0){
             printf("%s", line);
-            print_n = 1;
+            print_n = true;
         } else {
-            print_n = 0;
+            print_n = false;
         }
+
+        slice_start = 0;
         while (regex_success==0) {
             for(i = 0; i<pmatch.rm_eo; i++) {
                 if (i < pmatch.rm_so)
@@ -38,12 +41,15 @@ int main ( int argc , char * argv[])
                     printf("^");
             }
             // line = line[pmath.rm_eo:]
+            /*
             char * buffer = calloc(len, sizeof(char));
             for(i = pmatch.rm_eo; i<len; i++)
                 buffer[i-pmatch.rm_eo] = line[i];
             free(line);
             line = buffer;
-            regex_success = regexec(&regex, line, 1, &pmatch, 0); 
+            */
+            slice_start += pmatch.rm_eo;
+            regex_success = regexec(&regex, line+slice_start, 1, &pmatch, 0); 
         }
         if(print_n)
             printf("\n");
