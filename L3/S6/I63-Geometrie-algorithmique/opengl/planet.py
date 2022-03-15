dd86e2d3543f909e4669df03f71fec17fdaa42be
dxkkxn <ybkstudies@gmail.com>
2022-03-15 19:08:12 +0100

    finish opengl

--- L3/S6/I63-Geometrie-algorithmique/opengl/planet.py
+++ L3/S6/I63-Geometrie-algorithmique/opengl/planet.py
@@ -11,13 +11,12 @@ import sys
 
 ###############################################################
 # variables globales
-year, day = 0, 0
+year, day, hour,x_pos, y_pos, z_pos = 0, 0, 0, 5, 5, 5
 quadric = None
 
 ############################################################## # 
+blue = []
 
-blue = [1.0, 1.0, 1.0, 1.0]
-yellow = [1, 1, 0, 1]
 def init():
     global quadric
     # clear color to black
@@ -28,14 +27,17 @@ def init():
 #    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white)
 #    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, 9.0) 
 
-    glEnable(GL_LIGHTING)
-    glEnable(GL_COLOR_MATERIAL)
+    #glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LESS)
+    glClearDepth(1)
 
     glEnable(GL_LIGHT0)
     glLightfv(GL_LIGHT0, GL_POSITION, pos)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse)
-    glLightfv(GL_LIGHT0, GL_SPECULAR, specular)
-    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1)
+    glEnable(GL_LIGHTING)
+#    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse)
+#    glLightfv(GL_LIGHT0, GL_SPECULAR, specular)
+#    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1)
     glShadeModel(GL_SMOOTH)
 #    # definition of a quadric
     quadric = gluNewQuadric()
@@ -44,41 +46,80 @@ def init():
 
 
 def display():
+    #glClearDepth(0)
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor4f (1.0, 1.0, 1.0, 1.0)
+    #glColor4i (252, 236, 10, 255)
 
+    blue = [1.0, 1.0, 1.0, 1.0]
+    yellow = [0.7, .4, 0, 1]
     glPushMatrix()
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, yellow)
+    gluLookAt(x_pos, y_pos, z_pos, #pos camera 
+              -5+x_pos, -5+y_pos, -5+z_pos, # look at
+              0, 1, 0) #up vector
+    #glPushMatrix()
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1.0, 1.0, 0, 1.0])
     gluSphere(quadric, 1.0, 20, 16)
     glRotatef(year, 0.0, 1.0, 0.0)
     glTranslatef(2.0, 0.0, 0.0)
     glRotatef(day, 0.0, 1.0, 0.0)
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, blue)
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [91/255, 123/255, 227/255, 1.0])
     gluSphere(quadric, 0.2, 10, 8)
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [255/255, 255/255, 255/255, 1.0])
+    glRotatef(hour, 0.0, 1.0, 0.0)
+    glTranslatef(0.5, 0.0, 0.0)
+    gluSphere(quadric, 0.1, 10, 8)
+
     glPopMatrix()
 
     glutSwapBuffers()
+    #glFlush()
 
 def reshape(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     if width <= height:
-        glOrtho(-2.5, 2.5, -2.5*height/width, 2.5*height/width, -10.0, 10.0)
+        #glOrtho(-2.5, 2.5, -2.5*height/width, 2.5*height/width, -1000.0, 1000.0)
+        gluPerspective(100, width/height, 1, 1000)
     else:
         glOrtho(-2.5*width/height, 2.5*width/height, -2.5, 2.5, -10.0, 10.0)
     glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
 def keyboard(key, x, y):
-    global day, year
+    global day, year, hour, x_pos, y_pos, z_pos
     if key == b'j':
         day = (day + 10) % 360
 #    elif key == 'J':
 #        day = (day - 10) % 360
     elif key == b'a':
         year = (year + 5) % 360
+
+    elif key == b'h':
+        hour = (hour + 10) % 360
+
+    elif key == b't':
+        year = (year + 1) % 360
+        hour = (hour + 20) % 360
+        day = (day + 10) % 360
 #    elif key == 'A':
 #        year = (year - 5) % 360
+    elif key == b'x':
+        x_pos+=1
+    elif key == b'X':
+        x_pos-=1
+
+    elif key == b'y':
+        y_pos+= 1
+    elif key == b'Y':
+        y_pos-= 1
+
+    elif key == b'z':
+        z_pos += 1
+    elif key == b'Z':
+        z_pos -= 1
     elif key == b'\033':
         glutDestroyWindow(WIN)
         sys.exit(0)
@@ -92,12 +133,12 @@ if __name__ == "__main__":
     # initialization GLUT library
     glutInit()
     # initialization display mode RGBA mode and double buffered window
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA |GLUT_DEPTH | GLUT_LUMINANCE)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA |GLUT_DEPTH )
 
     # creation of top-level window
     WIN = glutCreateWindow('planet')
 
-    glutReshapeWindow(512,512)
+    glutReshapeWindow(512, 512)
 
     glutReshapeFunc(reshape)
     glutDisplayFunc(display)
